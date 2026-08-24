fabed299049f2b302d8097d79a017bf25ef9139c
Aurora <2575380562@qq.com>
2026-08-24 21:19:43 +0800

    refactor: export path rules

--- src/engine/__init__.py
+++ src/engine/__init__.py
@@ -4,6 +4,7 @@ from src.engine.board import Position
 from src.engine.piece import Piece, piece_from_char
 from src.engine.coordinates import BoardCoord, Square5D, Vector4D
 from src.engine.piece_movement import PieceMovementRules
+from src.engine.path_rules import PathBlocker, PathBlockReason, PathRules
 from src.engine.move_generator import Move, MoveGenerator
 from src.engine.move_validator import MoveValidator
 from src.engine.timeline import Timeline, TimelineManager
